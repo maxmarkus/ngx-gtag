6085772965c0e801983a41c0de957523ebe25497
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2019-01-28 09:20:08 +0100

    removing unused directive input

--- src/lib/google-analytics-events/google-analytics-events.directive.ts
+++ src/lib/google-analytics-events/google-analytics-events.directive.ts
@@ -6,7 +6,6 @@ import { NgxGtagEventService } from './google-analytics-events.service';
 })
 export class GoogleAnalyticsEventsDirective implements AfterViewInit {
   @Input() gtagOn: string;
-  @Input() gtagTrackingId: string;
   @Input() gtagAction: string;
   @Input() gtagOptions: any;
 
@@ -14,12 +13,12 @@ export class GoogleAnalyticsEventsDirective implements AfterViewInit {
     private ngxGtagEventService: NgxGtagEventService,
     private renderer: Renderer2,
     private el: ElementRef
-  ) {}
+  ) { }
 
   ngAfterViewInit() {
     try {
       this.renderer.listen(this.el.nativeElement, this.gtagOn, () => {
-        this.ngxGtagEventService.event({ action: this.gtagAction || this.gtagOn, options: { ...this.gtagOptions}});
+        this.ngxGtagEventService.event({ action: this.gtagAction || this.gtagOn, options: { ...this.gtagOptions } });
       });
     } catch (err) {
       console.error(err);
